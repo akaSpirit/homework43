c59aa4c1a651a57496d7f8b8956429331541de85
Spirit <mcspiritkg@gmail.com>
2022-08-26 13:47:40 +0600

    completed task1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
@@ -22,47 +24,21 @@ public class Main {
     private static HttpServer makeServer() throws IOException {
         String host = "localhost";
         InetSocketAddress address = new InetSocketAddress(host, 9889);
-
         System.out.printf("Start the server at: http://%s:%s%n", address.getHostName(), address.getPort());
-
         HttpServer server = HttpServer.create(address, 50); //backlog - количество попыток.
         System.out.println("Successfully!");
-
         return server;
     }
 
     private static void initRoutes(HttpServer server) {
-        server.createContext("/", Main::handleRootRequest);
+        server.createContext("/", Main::handleIndexRequest);
         server.createContext("/apps/", Main::handleAppsRequest);
         server.createContext("/apps/profile/", Main::handleProfileRequest);
     }
 
-    private static void handleRootRequest(HttpExchange exchange) {
-        try {
-            exchange.getResponseHeaders().add("Context-Type", "text/plain; charset=UTF-8");
-            int responseCode = 200;
-            int length = 0;
-            exchange.sendResponseHeaders(responseCode, length);
-
-            try (PrintWriter writer = getWriterFrom(exchange)) {
-                String method = exchange.getRequestMethod();
-                URI uri = exchange.getRequestURI();
-                String ctxPath = exchange.getHttpContext().getPath();
-                write(writer, "HTTP method", method);
-                write(writer, "Root request", uri.toString());
-                write(writer, "Root processed through", ctxPath);
-                writeHeaders(writer, "Root request headers", exchange.getRequestHeaders());
-                writeData(writer, exchange);
-                writer.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     private static void handleAppsRequest(HttpExchange exchange) {
         try {
-            exchange.getResponseHeaders().add("Context-Type", "text/plain; charset=UTF-8");
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
             int responseCode = 200;
             int length = 0;
             exchange.sendResponseHeaders(responseCode, length);
@@ -85,7 +61,7 @@ public class Main {
 
     private static void handleProfileRequest(HttpExchange exchange) {
         try {
-            exchange.getResponseHeaders().add("Context-Type", "text/plain; charset=UTF-8");
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
             int responseCode = 200;
             int length = 0;
             exchange.sendResponseHeaders(responseCode, length);
@@ -106,6 +82,45 @@ public class Main {
         }
     }
 
+    private static void handleIndexRequest(HttpExchange exchange) {
+        Path path = Paths.get("./index.html");
+        if (Files.exists(path)) {
+            System.out.println("Exists");
+            try {
+//                exchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF_8");
+                exchange.getResponseHeaders().add("Content-Type", "text/css");
+                int responseCode = 200;
+                String response = readFile(path, StandardCharsets.UTF_8);
+                exchange.sendResponseHeaders(responseCode, response.length());
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+
+                try (PrintWriter writer = getWriterFrom(exchange)) {
+                    String method = exchange.getRequestMethod();
+                    URI uri = exchange.getRequestURI();
+                    String ctxPath = exchange.getHttpContext().getPath();
+                    write(writer, "HTTP method", method);
+                    write(writer, "Index request", uri.toString());
+                    write(writer, "Index processed through", ctxPath);
+                    writeHeaders(writer, "Index request headers", exchange.getRequestHeaders());
+                    writeData(writer, exchange);
+                    writer.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+            System.out.println("Does not Exists");
+    }
+
+    static String readFile(Path path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(path);
+        return new String(encoded, encoding);
+    }
+
     private static PrintWriter getWriterFrom(HttpExchange exchange) {
         OutputStream output = exchange.getResponseBody();
         Charset charset = StandardCharsets.UTF_8;
@@ -114,7 +129,6 @@ public class Main {
 
     private static void write(Writer writer, String msg, String method) {
         String data = String.format("%s: %s%n%n", msg, method);
-
         try {
             writer.write(data);
         } catch (Exception e) {
@@ -142,7 +156,6 @@ public class Main {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
     }
 
 }
